51727c1e53d91370107816373988a72b90c4819a
Patrick Bajao <earl.bajao@gmail.com>
2013-03-02 10:34:22 +0800

    Adds apache-ant to development packages

--- scripts/install_development_tools.sh
+++ scripts/install_development_tools.sh
@@ -5,5 +5,5 @@ mkdir $HOME/Development
 echo "Done."
 
 echo "Downloading requirements..."
-sudo pacman -S vim curl git lib32-glibc lib32-libstdc++5 lib32-zlib lib32-ncurses
+sudo pacman -S vim curl git apache-ant lib32-glibc lib32-libstdc++5 lib32-zlib lib32-ncurses
 echo "Done."
